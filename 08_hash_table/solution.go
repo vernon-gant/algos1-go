0dd6df9d161f335c6dff920b549e1b5421cd600f
Aleksandr Zakharov <aleks.zakharov@pm.me>
2025-12-21 00:15:46 +0300

    template

---
+++ 08_hash_table/solution.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"strconv"
+	"os"
+)
+
+type HashTable struct {
+	size  int
+	step  int
+	slots []string
+}
+
+func Init(sz int, stp int) HashTable {
+	ht := HashTable{size: sz, step: stp, slots: nil}
+	ht.slots = make([]string, sz)
+	return ht
+}
+
+func (ht *HashTable) HashFun(value string) int {
+	return 0
+}
+
+func (ht *HashTable) SeekSlot(value string) int {
+	return -1
+}
+
+func (ht *HashTable) Put(value string) int {
+	return -1
+}
+
+func (ht *HashTable) Find(value string) int {
+	return -1
+}
